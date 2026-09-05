fdd583a827c7fbb1664102c799183fafcb5452e5
akashhsingh1906-lab <akashhsingh1906@gmail.com>
2026-09-05 12:53:25 +0530

    Add project 2 readme

--- 1.29_nested_functions.sql
+++ 1.29_nested_functions.sql
@@ -7,3 +7,21 @@ union all
 select 'sql'
 union all
 select 'r';
+
+-- covert to ctas
+
+with skills as(
+    select 'python' as skill 
+    union all
+    select 'sql'
+    union all
+    select 'r'
+)
+select skill
+from skills;
+
+
+-- struct
+
+select { skill : 'python', level: 1 } as skill_struct;
+
